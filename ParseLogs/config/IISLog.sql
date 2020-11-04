af414c4bd35b5c4549c701271f90945c5fbb935f
shinylight <2530444+shinylightdev@users.noreply.github.com>
2020-11-04 18:32:06 -0500

    New SQL File

---
+++ ParseLogs/config/IISLog.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[IISLog](
+	[logfile] [NVARCHAR](255) NULL,
+	[datestamp] [DATETIME] NULL,
+	[cs_method] [NVARCHAR](255) NULL,
+	[cs_uri_stem] [NVARCHAR](4000) NULL,
+	[cs_uri_query] [NVARCHAR](4000) NULL,
+	[cs_username] [NVARCHAR](255) NULL,
+	[c_ip] [NVARCHAR](255) NULL,
+	[cs_User_Agent] [NVARCHAR](4000) NULL,
+	[cs_referer] [NVARCHAR](4000) NULL,
+	[sc_status] [NVARCHAR](255) NULL,
+	[sc_win32_status] [NVARCHAR](255) NULL,
+	[time_taken_ms] [INT] NULL
+) ON [PRIMARY]
+GO
